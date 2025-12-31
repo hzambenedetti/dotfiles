9eeaa8562958d8c0f79e4252e7c5ce3388e8ad45
hzambenedetti <hique.rz03@gmail.com>
2025-12-30 22:49:19 -0300

    refact(nvim): Refact treesitter config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,28 +1,37 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter 
 local function config()
+  local parsers = {
+    "c",
+    "lua",
+    "vim",
+    "vimdoc",
+    "query",
+    "markdown",
+    "rust",
+    "python",
+    "go",
+    "typescript",
+    "javascript"
+  }
 
-  local status_ok, configs = pcall(require, "nvim-treesitter.configs")
-
+  local status_ok, ts = pcall(require, "nvim-treesitter")
   if not status_ok then
     return
   end
 
-  configs.setup({
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown" },
-    sync_install = false,
-    auto_install = true,
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-    indent = { enable = true },
-  })
+  ts.install(parsers)
+
+  vim.api.nvim_create_autocmd('FileType', {
+  pattern = { '<filetype>' },
+  callback = function() vim.treesitter.start() end,
+})
 end
 
 -- Return the Lazy spec
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     build = ":TSUpdate",
     config = config, -- Reference the function here
   }
